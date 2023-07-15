3ade69a99171754bee241dbbb2e620929d3c3a92
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2023-07-15 15:53:40 -0300

    Modulo Orientação a Objetos

---
+++ Modulo2/meu_app/bin/meu_app.dart
@@ -0,0 +1,28 @@
+import '../lib/loops/for.dart' as app_for;
+import '../lib/loops/for_each.dart' as app_for_each;
+import '../lib/loops/while.dart' as app_while;
+import '../lib/loops/do_while.dart' as app_do_while;
+import '../lib/functions/functions.dart' as app_functions;
+import '../lib/functions/functions_2.dart' as app_functions_2;
+import '../lib/functions/functions_3.dart' as app_functions_3;
+import '../lib/functions/functions_4_fatorial.dart'
+    as app_functions_4_fatorial;
+import '../lib/functions/functions_5_optional.dart'
+    as functions_5_optional;
+import '../lib/functions/functions_6_arrow.dart' as functions_6_arrow;
+import '../lib/functions/functions_7_function_as_parameter.dart'
+    as functions_7_function_as_parameter;
+
+void main(List<String> arguments) {
+  //app_for.main(arguments);
+  //app_for_each.main(arguments);
+  //app_while.main(arguments);
+  //app_do_while.main(arguments);
+  //app_functions.main(arguments);
+  //app_functions_2.main(arguments);
+  //app_functions_3.main(arguments);
+  //app_functions_4_fatorial.main(arguments);
+  //functions_5_optional.main(arguments);
+  //functions_6_arrow.main(arguments);
+  functions_7_function_as_parameter.main(arguments);
+}
